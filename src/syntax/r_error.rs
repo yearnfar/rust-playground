80bfdf5e972017f93ca60ee501790597f2fc337c
Yu Huaping <huaping.yu@mintegral.com>
2023-12-11 00:39:45 +0800

    feat: error

--- src/syntax/r_error.rs
+++ src/syntax/r_error.rs
@@ -2,6 +2,13 @@ fn run_panic() {
     panic!("test panic");
 }
 
+fn run_result_ok() -> Result<i32, ()> {
+    Ok(1)
+}
+fn run_result_err() -> Result<i32, String> {
+    Err(String::from("some thing wrong"))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -9,5 +16,26 @@ mod tests {
     #[test]
     fn test_run_panic() {
         run_panic();
+    }
+
+    #[test]
+    fn test_run_result_ok() {
+        let ret = match run_result_ok() {
+            Ok(n) => n,
+            Err(()) => 0,
+        };
+        println!("ret: {}", ret)
+    }
+
+    #[test]
+    fn test_run_result_err() {
+        let ret = match run_result_err() {
+            Ok(n) => n,
+            Err(err) => {
+                println!("msg: {}", err);
+                0
+            }
+        };
+        println!("ret: {}", ret)
     }
 }
