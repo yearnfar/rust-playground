d4c82aba6cc373cbdcbea66e21e4b7ba8e4f293c
yearnfar <yearnfar@gmail.com>
2023-12-14 02:24:42 +0800

    feat: hashmap

--- src/syntax/r_hashmap.rs
+++ src/syntax/r_hashmap.rs
@@ -2,10 +2,11 @@ use std::collections::HashMap;
 
 fn run() {
     let mut scores = HashMap::new();
-    scores.insert("k2", "");
+    scores.insert("k2", "v2");
     scores.insert("k3", "v3");
 
     scores.entry("k2").or_insert("v2_2");
+    scores.entry("k1").or_insert("v1");
 
     println!("{:?}", scores);
 
